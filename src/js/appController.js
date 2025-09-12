47b8edb7ec901f5e8cf4f358bba5b9cb75e2efe3
aryan1403 <aaryan14032006@email.com>
2025-09-12 14:56:33 +0530

    done

--- src/js/appController.js
+++ src/js/appController.js
@@ -1,13 +1,3 @@
-/**
- * @license
- * Copyright (c) 2014, 2025, Oracle and/or its affiliates.
- * Licensed under The Universal Permissive License (UPL), Version 1.0
- * as shown at https://oss.oracle.com/licenses/upl/
- * @ignore
- */
-/*
- * Your application specific code will go here
- */
 define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojknockout'],
   function(ko, moduleUtils) {
      function ControllerViewModel() {
